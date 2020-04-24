da3c5ae96f5463069143bd5b73dcece24c708765
michel24000 <michel.cardon@edmond-labbe.fr>
2020-04-24 10:04:55 +0200

    ajout de l'envoi de mail via ajax

---
+++ test.js
@@ -0,0 +1,20 @@
+//au load de la page
+$(document).ready(function () {
+    $("#myform input.style1").click(function (event) {
+        event.preventDefault();
+        console.log('click ! ');
+
+        var name = $("#myform #contact-name").val();
+        var email = $("#myform #contact-email").val();
+        var message = $("#myform #contact-message").val();
+
+        //appel ajax
+        $.ajax({
+            url:"emailThis.php",
+            method:"POST",
+            data:{name: name, email: email, message: message}
+        }).done(function () {
+           alert("message envoyé");
+        });
+    });
+});
